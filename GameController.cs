9c0ecd1ec25ed5b3d91edc414417924cf6ea4222
Ash Edwards <8330786+AshEdwardsDesign@users.noreply.github.com>
2019-08-19 12:04:18 +0100

    more work on reqorking winning move logic

--- GameController.cs
+++ GameController.cs
@@ -7,119 +7,142 @@ namespace Connect_4
         public static Player player1;
         public static Player player2;
 
+        // 
+        // Nearby board slots
+        //
+
+        // Pieces to the left
+        static GamePiece oneToLeft;
+        static GamePiece twoToLeft;
+        static GamePiece threeToLeft;
+        // Pieces to the right
+        static GamePiece oneToRight;
+        static GamePiece twoToRight;
+        static GamePiece threeToRight;
+        // Pieces above
+        static GamePiece oneAbove;
+        static GamePiece twoAbove;
+        static GamePiece threeAbove;
+        // Pieces below
+        static GamePiece oneBelow;
+        static GamePiece twoBelow;
+        static GamePiece threeBelow;
+
+        // TO DO: Horizontal slots
+
         public static bool isWinningMove(Player player, GamePiece[,] board, int col, int row)
         {
-
-            // 
-            // Nearby board slots
-            //
-
-            // Pieces to the left
-            GamePiece oneToLeft = board[row, col - 1].GetBoardGamePiece();
-            GamePiece twoToLeft = board[row, col - 2].GetBoardGamePiece();
-            GamePiece threeToLeft = board[row, col - 3].GetBoardGamePiece();
-            // Pieces to the right
-            GamePiece oneToRight = board[row, col + 1].GetBoardGamePiece();
-            GamePiece twoToRight = board[row, col + 2].GetBoardGamePiece();
-            GamePiece threeToRight = board[row, col + 3].GetBoardGamePiece();
-            // Pieces above
-            GamePiece oneAbove = board[row - 1, col].GetBoardGamePiece();
-            GamePiece twoAbove = board[row - 2, col].GetBoardGamePiece();
-            GamePiece threeAbove = board[row - 3, col].GetBoardGamePiece();
-            // Pieces below
-            GamePiece oneBelow = board[row + 1, col].GetBoardGamePiece();
-            GamePiece twoBelow = board[row + 2, col].GetBoardGamePiece();
-            GamePiece threeBelow = board[row + 3, col].GetBoardGamePiece();
-
-            //
-            // HORIZONTAL MOVE CHECKING
-            //
-
-            try
+            GetNearbyPieces(board, col, row);
+            // If pieces to left are valid...
+            if (oneToLeft != null && twoToLeft != null && threeToLeft != null)
             {
-                // Check the 3 pieces to the left
-                if (board[row, col - 1] == player.GetGamePiece()
-                    && board[row, col -2] == player.GetGamePiece()
-                    && board[row, col - 3] == player.GetGamePiece())
-                {
-                    return true;
-                }
+                if ()
             }
-            catch (Exception)
-            {
-
-                Console.WriteLine("Error checking nearby pieces");
-            }
-
-            try
-            {
-                // Check 2 to the left and 1 to the right
-                if (board[row, col - 2] == player.GetGamePiece()
-                    && board[row, col - 1] == player.GetGamePiece()
-                    && board[row, col + 1] == player.GetGamePiece())
-                {
-                    return true;
-                }
-            }
-            catch (Exception)
-            {
-
-                Console.WriteLine("Error checking nearby pieces");
-            }
-
-            try
-            {
-                // Check 1 to the left and 2 to the right
-                if (board[row, col - 1] == player.GetGamePiece()
-                    && board[row, col + 1] == player.GetGamePiece()
-                    && board[row, col + 2] == player.GetGamePiece())
-                {
-                    return true;
-                }
-            }
-            catch (Exception)
-            {
-
-                Console.WriteLine("Error checking nearby pieces");
-            }
-
-            try
-            {
-                // Check 3 to the right
-                if (board[row, col + 1] == player.GetGamePiece()
-                    && board[row, col + 2] == player.GetGamePiece()
-                    && board[row, col + 3] == player.GetGamePiece())
-                {
-                    return true;
-                }
-            }
-            catch (Exception)
-            {
-
-                Console.WriteLine("Error checking nearby pieces");
-            }
-
-            //
-            // VERTICAL MOVE CHECKING
-            //
-
-            try
-            {
-                // Check 3 below
-                if (board[row - 1, col] == player.GetGamePiece()
-                    && board[row - 2, col] == player.GetGamePiece()
-                    && board[row - 3, col] == player.GetGamePiece())
-                {
-                    return true;
-                }
-            }
-            catch (Exception)
-            {
-
-                Console.WriteLine("Error checking nearby pieces");
-            }
-
             return false;
+        }
+
+        public static void GetNearbyPieces(GamePiece[,] board, int col, int row)
+        {
+            // Pieces to the left
+            try
+            {
+                oneToLeft = board[row, col - 1].GetBoardGamePiece();
+            }
+            catch
+            {
+                oneToLeft = null;
+            }
+            try
+            {
+                twoToLeft = board[row, col - 2].GetBoardGamePiece();
+            }
+            catch
+            {
+                twoToLeft = null;
+            }
+            try
+            {
+                threeToLeft = board[row, col - 3].GetBoardGamePiece();
+            }
+            catch
+            {
+                threeToLeft = null;
+            }
+            // Pieces to the right
+            try
+            {
+                oneToRight = board[row, col + 1].GetBoardGamePiece();
+            }
+            catch
+            {
+                oneToRight = null;
+            }
+            try
+            {
+                twoToRight = board[row, col + 2].GetBoardGamePiece();
+            }
+            catch
+            {
+                twoToRight = null;
+            }
+            try
+            {
+                threeToRight = board[row, col + 3].GetBoardGamePiece();
+            }
+            catch
+            {
+                threeToRight = null;
+            }
+            // Pieces above
+            try
+            {
+                oneAbove = board[row - 1, col].GetBoardGamePiece();
+            }
+            catch
+            {
+                oneAbove = null;
+            }
+            try
+            {
+                twoAbove = board[row - 2, col].GetBoardGamePiece();
+            }
+            catch
+            {
+                twoAbove = null;
+            }
+            try
+            {
+                threeAbove = board[row - 3, col].GetBoardGamePiece();
+            }
+            catch
+            {
+                threeAbove = null;
+            }
+            // Pieces below
+            try
+            {
+                oneBelow = board[row + 1, col].GetBoardGamePiece();
+            }
+            catch
+            {
+                oneBelow = null;
+            }
+            try
+            {
+                twoBelow = board[row + 2, col].GetBoardGamePiece();
+            }
+            catch
+            {
+                twoBelow = null;
+            }
+            try
+            {
+                threeBelow = board[row + 3, col].GetBoardGamePiece();
+            }
+            catch
+            {
+                threeBelow = null;
+            }
         }
     }
 }
